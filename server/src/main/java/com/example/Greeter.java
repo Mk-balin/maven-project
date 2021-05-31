44a0c6600dc00262bc224b2f6d2200d4e23434d4
Mk-balin <80404886+Mk-balin@users.noreply.github.com>
2021-05-31 16:27:55 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,17 +1,15 @@
 package com.example;
 
 public class Greeter {
-
+/** ceci est la class greeter */
   
   public Greeter() {
 
 /** {@inheritDoc} */
-public int checkReturnTag(final int aTagIndex,
-                          JavadocTag[] aTags,
-                          int aLineNo)
   }
 
   public final String greet(final String someone) {
+    /** ceci est la methode greet */
     return String.format("Hello babacar, %s!", someone);
   }
 }
