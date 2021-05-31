988e123d6ceda1cf717656df2b10def012500181
Mk-balin <80404886+Mk-balin@users.noreply.github.com>
2021-05-31 16:32:09 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,8 +7,8 @@ public class Greeter {
   }
   public final String greet(final String someone) {
     /** ceci est la methode greet 
-    *@param someone c'est le nom de la personne
-    *@return c'est la phrase de retour
+    * @param someone c'est le nom de la personne
+    * @return c'est la phrase de retour
     */
     return String.format("Hello babacar, %s!", someone);
   }
