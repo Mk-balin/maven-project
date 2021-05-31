d01e91b01035e28c5e7dc329717b5c8fc5f124b0
Mk-balin <80404886+Mk-balin@users.noreply.github.com>
2021-05-31 16:36:39 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,13 @@
-/* ceci est le @package */
+/* ceci est le @package. */
 package com.example;
 
 public class Greeter {
-/** ceci est la class greeter */
+/** ceci est la class greeter. */
   public Greeter() {
-/** ceci est un constructeur*/
+/** ceci est un constructeur. */
   }
   public final String greet(final String someone) {
-    /** ceci est la methode greet 
+    /** ceci est la methode greet.
     * @param someone c'est le nom de la personne
     * @return c'est la phrase de retour
     */
